939b74a0694f472506c3b52866aae9c2309bf4b6
rutgervenekatte <rutgervenekatte@gmail.com>
2026-07-16 23:33:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,51 @@ class CampScene extends Phaser.Scene{
     create(){
 
         this.cameras.main.setBackgroundColor("#63c76a");
+// Wereld vergroten
+this.physics.world.setBounds(0, 0, 2000, 2000);
 
+// Gras
+const grass = this.add.graphics();
+
+grass.fillStyle(0x63c76a, 1);
+grass.fillRect(0, 0, 2000, 2000);
+
+// Pad
+grass.fillStyle(0xc9b07b, 1);
+grass.fillRect(250, 820, 1500, 180);
+
+// Meer
+grass.fillStyle(0x4ea8de, 1);
+grass.fillEllipse(1650, 1450, 500, 350);
+
+// Kampvuur
+grass.fillStyle(0xff7b00, 1);
+grass.fillCircle(980, 940, 18);
+
+// Kantine
+grass.fillStyle(0x6b4f2a, 1);
+grass.fillRect(1180, 640, 240, 170);
+
+// Tenten
+grass.fillStyle(0xd9d9d9, 1);
+
+grass.fillTriangle(
+300,350,
+380,430,
+220,430
+);
+
+grass.fillTriangle(
+650,300,
+730,380,
+570,380
+);
+
+grass.fillTriangle(
+980,340,
+1060,420,
+900,420
+);
         this.add.text(
             20,
             20,
@@ -22,7 +66,15 @@ class CampScene extends Phaser.Scene{
             }
         );
 
-        this.player=this.add.circle(
+        this.player = this.physics.add.image(
+400,
+900,
+null
+);
+
+this.player.setCircle(18);
+
+this.player.setTint(0x3498db);
             400,
             300,
             18,
